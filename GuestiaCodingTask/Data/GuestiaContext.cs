2ad13957a69d1bfeb457b165c0a449ae2c62b3fd
Ben Gould <ben@amb1ent.org>
2024-10-17 14:41:42 +0100

    Run sqlexpress via docker compose.

--- GuestiaCodingTask/Data/GuestiaContext.cs
+++ GuestiaCodingTask/Data/GuestiaContext.cs
@@ -9,7 +9,8 @@ namespace GuestiaCodingTask.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=GuestiaDB;Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=GuestiaDB;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=master;Initial Catalog=master;Persist Security Info=False;User Id=sa;Password=Guestia123!;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;Connection Timeout=30;");
         }
     }
 }
